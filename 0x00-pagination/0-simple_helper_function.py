900638d72803aefe8afc062cd63fee96ed74287e
Winfred <winfrednginakilonzo@gmail.com>
2023-12-29 13:52:20 +0300

    Import tuple from typing

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """return in a list for those particular pagination parameters"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of size two containing
     a start index and an end index"""
     start_index = (page - 1) * page_size
