c8d2cb130d4e765f579791d3946c8b703c1e88d5
Winfred <winfrednginakilonzo@gmail.com>
2024-01-09 22:48:09 +0300

    Provide language locale

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Basic Flask app"""
+from flask import Flask, render_template, g, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Language and time configuration class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages"""
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Returns index.html"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- 0x02-i18n/README.md
+++ 0x02-i18n/README.md
@@ -1,1 +1,8 @@
-i18n
+ jinja2.ext.autoescape and jinja2.ext.with_ extensions are deprecated in Jinja2 version 3.0.0 and later. These extensions have been added to the compiler in these versions 1.
+
+You should remove these extensions from your babel.cfg file. Your babel.cfg file should look like this:
+
+[python: **.py]
+[jinja2: **/templates/**.html]
+;extensions=jinja2.ext.autoescape,jinja2.ext.with_
+
