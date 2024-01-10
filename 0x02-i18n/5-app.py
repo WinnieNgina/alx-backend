f9cb67f40a40ce156f880a1ebb7d03329102be35
Winfred <winfrednginakilonzo@gmail.com>
2024-01-10 10:22:31 +0300

    Fixed code

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -53,7 +53,7 @@ def get_locale():
 @app.route('/')
 def index():
     """Returns index.html"""
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
