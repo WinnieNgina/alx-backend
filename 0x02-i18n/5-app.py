528d50b49ef462cc395af1892b8db644dfda430d
Winfred <winfrednginakilonzo@gmail.com>
2024-01-10 07:39:10 +0300

    Mock user

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" Basic Flask app"""
+from flask import Flask, render_template, g, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """Get the current user from the users table"""
+    user_id = request.args.get('login_as')
+    if user_id and int(user_id) in users:
+        return users[int(user_id)]
+    return None
+
+
+@app.before_request
+def before_request():
+    """Set the current user before each request"""
+    g.user = get_user()
+
+
+class Config:
+    """Language and time configuration class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages"""
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Returns index.html"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
